52493ac9ce434174a81e1996c07e7eac404b413c
Ishmael <64021138+Ishmaelsealey@users.noreply.github.com>
2023-09-11 04:59:14 -0400

    annotated javascript file

--- src/script.js
+++ src/script.js
@@ -1,18 +1,26 @@
+// select the html elements used in the program
+
 const headingId = document.querySelector('#js-advice-id');
 const advice = document.querySelector('#js-advice');
 const newAdviceBtn = document.querySelector('#js-dice-btn');
 
 function getNewAdvice() {
-	console.log('Hello World!')
+
+	// Get the advice slip from api
 	fetch('https://api.adviceslip.com/advice')
+
+	// Make the response a json object
 	.then(response => response.json())
+
+	// Using the json response, edit the html page
 	.then(data => {
-		console.log(data.slip)
 		headingId.innerHTML = data.slip.id
 		advice.innerHTML = `"${data.slip.advice}"`
 	})
 }
 
+// run function when dice button is clicked
 newAdviceBtn.addEventListener('click', getNewAdvice)
 
+// run function upon initial page load
 document.addEventListener('DOMContentLoaded', getNewAdvice)
